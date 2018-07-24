c4a0f75ca95ab6042d6ebedf3646f4c71723f8b6
ahmedHarby33 <ah0121017@gmail.com>
2018-07-24 14:51:37 +0200

    education view

---
+++ resources/views/backend/studyphase.blade.php
@@ -0,0 +1,48 @@
+
+
+
+@extends('backend.layout.master')
+
+@section('content')
+
+
+
+<div class="content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="title">اضافه مراحل الدراسية</div> 
+                            </div>
+                            <div class="card-body">
+
+
+                              @include('alerts')
+                              <form method="post" action="http://localhost/OurSchool/public/xxxxx">
+                                  {{csrf_field()}}
+                                  <div class="form-group row">
+                                    <label for="example-text-input" class="col-2 col-form-label">اسم المرحلة الدراسية</label>
+                                    <div class="col-10">
+                                        <input class="form-control" type="text" value="اسم المرحلة الدراسية" id="name" class="name">
+                                    </div>
+                                </div>
+
+
+
+                    
+                              <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">حفظ </button>
+                                     <button type="submit" class="btn btn-inverse waves-effect waves-light">الغاء</button>
+
+
+                                  
+                                  
+                                  
+                                    
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div> 
+@endsection
